12a16103a00237d762db707e9c0acf798cb7a591
Steivin <steivin2000@gmail.com>
2024-05-29 22:19:41 -0500

    javascript file for the index

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Definición de la función load_page
+function load_page() {
+    Swal.fire({
+        title: "RICHARD MILLE",
+        text: "Usa lo mejor, así como Rafa",
+        imageUrl: "https://hips.hearstapps.com/hmg-prod/images/rafa-nadal-reloj-richard-mille-open-australia-1642527171.png?crop=0.501xw:1.00xh;0.160xw,0&resize=1200:*",
+        imageWidth: 500,
+        imageHeight: 400,
+        showConfirmButton: false,
+        timer: 3000,
+    });
+}
